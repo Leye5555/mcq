33427f3781bdab74dfc66b36c4b435d1075534a7
Olubummo Faith <59509795+Leye5555@users.noreply.github.com>
2025-03-19 06:23:12 +0000

    pre-deployment

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import { Smooch_Sans } from "next/font/google";
+import Link from "next/link";
 const font = Smooch_Sans({ subsets: ["latin"] });
 const Header = () => {
   return (
     <div className="mb-10 gap-2 flex items-center">
       <Image src="/assets/logo.svg" width={32} height={32} alt="mcq" />{" "}
-      <h1
-        className={`${font.className} ` + "text-white font-extrabold text-3xl"}
-      >
-        ulster_mcq
-      </h1>
+      <Link href="/">
+        <h1
+          className={
+            `${font.className} ` + "text-white font-extrabold text-3xl"
+          }
+        >
+          ulster_mcq
+        </h1>
+      </Link>
     </div>
   );
 };
